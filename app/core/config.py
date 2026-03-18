09aa5f68afb0ff821dbd8be1cdd2b4045bf1e490
vockagolordy <162159968+vockagolordy@users.noreply.github.com>
2026-03-18 20:52:27 +0300

    feat(db): set up env properties and connection to DB(#16) (#23)

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def database_url(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
