eda3736f0908fd4bc02c240ba0f8390b4868cabd
ropoy <iramyruanova@gmail.com>
2026-05-15 11:45:03 +0300

    fix: minor fix

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,15 +1,15 @@
 import multiprocessing
-import os
 
 bind = '0.0.0.0:8000'
 worker_class = 'uvicorn.workers.UvicornWorker'
-workers = int(os.getenv('WEB_CONCURRENCY', multiprocessing.cpu_count() * 2 + 1))
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_connections = 1000
 
-timeout = int(os.getenv('GUNICORN_TIMEOUT', '60'))
-graceful_timeout = int(os.getenv('GUNICORN_GRACEFUL_TIMEOUT', '30'))
-keepalive = int(os.getenv('GUNICORN_KEEPALIVE', '5'))
-
-accesslog = os.getenv('GUNICORN_ACCESS_LOG', '-')
-errorlog = os.getenv('GUNICORN_ERROR_LOG', '-')
-loglevel = os.getenv('GUNICORN_LOG_LEVEL', 'info')
-reload = os.getenv('GUNICORN_RELOAD', 'false').lower() in {'1', 'true', 'yes', 'on'}
+max_requests = 1000
+max_requests_jitter = 50
+timeout = 30
+graceful_timeout = 30
+keepalive = 5
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
+reload_app = True
+preload_app = True
